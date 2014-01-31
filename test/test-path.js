c42fe14283ecba4f427e0fb106ce14fe9c5df3b7
1000ch <orz1000ch@gmail.com>
2014-01-31 18:12:17 +0900

    update test

--- test/test-path.js
+++ test/test-path.js
@@ -10,40 +10,46 @@ describe('pngcrush()', function () {
     fs.unlinkSync('test/fixtures/minified.png');
   });
 
-  it('should return path to pngcrush binary', function (cb) {
+  it('should return path to pngcrush binary', function (callback) {
     var binPath = require('../lib/pngcrush').path;
 
     execFile(binPath, ['-v', '-'], function (err, stdout, stderr) {
       assert(stderr.toString().indexOf('pngcrush') !== -1);
-      cb();
+      callback();
     });
   });
 
-  it('should successfully proxy pngcrush', function (cb) {
+  it('should successfully proxy pngcrush', function (callback) {
     var binPath = path.join(__dirname, '../bin/pngcrush.js');
 
     execFile('node', [binPath, '-v', '-'], function (err, stdout, stderr) {
       assert(stderr.toString().indexOf('pngcrush') !== -1);
-      cb();
+      callback();
     });
   });
 
-  it('should minify a .png', function (cb) {
-    var binPath = path.join(__dirname, '../bin/pngcrush.js');
-    var args = [
-      '-rem alla',
-      '-brute',
-      '-reduce',
-      path.join(__dirname, 'fixtures', 'test.png'),
-      path.join(__dirname, 'fixtures', 'minified.png')
-    ];
+  it('should minify a .png', function (callback) {
+    
+    var rs = fs.createReadStream(path.join(__dirname, 'fixtures', 'test.png'));
+    var ws = fs.createWriteStream(path.join(__dirname, 'fixtures', 'minified.png'));
+    
+    ws.on('close', function () {
+      var binPath = path.join(__dirname, '../bin/pngcrush.js');
+      var args = [
+        '-rem alla',
+        '-brute',
+        '-reduce',
+        path.join(__dirname, 'fixtures', 'minified.png')
+      ];
 
-    execFile('node', [binPath].concat(args), function () {
-      var actual = fs.statSync('test/fixtures/minified.png').size;
-      var original = fs.statSync('test/fixtures/test.png').size;
-
-      assert(actual < original);
-      cb();
+      execFile('node', [binPath].concat(args), function () {
+        var actual = fs.statSync('test/fixtures/minified.png').size;
+        var original = fs.statSync('test/fixtures/test.png').size;
+        assert(actual < original);
+        callback();
+      });
     });
+    
+    rs.pipe(ws);
   });
 });
