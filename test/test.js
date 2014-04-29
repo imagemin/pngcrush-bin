4b858e4c86db37690e8baf3e50221cfb46c9dc73
Kevin Martensson <kevinmartensson@gmail.com>
2014-04-29 13:41:24 +0200

    Use bin-wrapper 0.3.0

---
+++ test/test.js
@@ -0,0 +1,64 @@
+/*global afterEach, beforeEach, describe, it */
+'use strict';
+
+var assert = require('assert');
+var binCheck = require('bin-check');
+var BinBuild = require('bin-build');
+var execFile = require('child_process').execFile;
+var fs = require('fs');
+var path = require('path');
+var rm = require('rimraf');
+
+describe('pngcrush()', function () {
+  afterEach(function (cb) {
+    rm(path.join(__dirname, 'tmp'), cb);
+  });
+
+  beforeEach(function () {
+    fs.mkdirSync(path.join(__dirname, 'tmp'));
+  });
+
+  it('should rebuild the pngcrush binaries', function (cb) {
+    var tmp = path.join(__dirname, 'tmp');
+    var builder = new BinBuild()
+      .src('http://downloads.sourceforge.net/project/pmt/pngcrush/1.7.73/pngcrush-1.7.73.zip')
+      .make('make && mv ./pngcrush ' + path.join(tmp, 'pngcrush'));
+
+    builder.build(function (err) {
+      assert(!err);
+      assert(fs.existsSync(path.join(tmp, 'pngcrush')));
+      cb();
+    });
+  });
+
+  it('should return path to binary and verify that it is working', function (cb) {
+    var binPath = require('../').path;
+    var args = [
+      '-reduce',
+      '-brute',
+      path.join(__dirname, 'fixtures/test.png'),
+      path.join(__dirname, 'tmp/test.png')
+    ];
+
+    binCheck(binPath, args, function (err, works) {
+      cb(assert.equal(works, true));
+    });
+  });
+
+  it('should minify a PNG', function (cb) {
+    var binPath = require('../').path;
+    var args = [
+      '--recompress',
+      '--shrink-extra',
+      path.join(__dirname, 'fixtures/test.png'),
+      path.join(__dirname, 'tmp/test.png')
+    ];
+
+    execFile(binPath, args, function () {
+      var src = fs.statSync(path.join(__dirname, 'fixtures/test.png')).size;
+      var dest = fs.statSync(path.join(__dirname, 'tmp/test.png')).size;
+
+      cb(assert(dest < src));
+    });
+  });
+});
