ffdaf76dcdc716ecf9e4c96fd2ce2de533e6ae51
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-20 17:05:33 +0200

    Create and remove the tmp directory before and after tests

--- test/test.js
+++ test/test.js
@@ -4,33 +4,24 @@ var binCheck = require('bin-check');
 var BinBuild = require('bin-build');
 var execFile = require('child_process').execFile;
 var fs = require('fs');
-var mkdir = require('mkdirp');
 var path = require('path');
-var rm = require('rimraf');
 var test = require('ava');
 var tmp = path.join(__dirname, 'tmp');
 
 test('rebuild the pngcrush binaries', function (t) {
-	t.plan(3);
+	t.plan(2);
 
-	var move = process.platform === 'win32' ? 'move' : 'mv';
-	var name = process.platform === 'win32' ? 'pngcrush.exe' : 'pngcrush';
 	var version = require('../').version;
-
 	var builder = new BinBuild()
 		.src('http://downloads.sourceforge.net/project/pmt/pngcrush/' + version + '/pngcrush-' + version + '.zip')
 		.cmd('mkdir -p ' + tmp)
-		.cmd('make && ' + move + ' ' + name + ' ' + path.join(tmp, name));
+		.cmd('make && mv pngcrush ' + path.join(tmp, 'pngcrush'));
 
 	builder.run(function (err) {
 		t.assert(!err);
 
-		fs.exists(path.join(tmp, name), function (exists) {
+		fs.exists(path.join(tmp, 'pngcrush'), function (exists) {
 			t.assert(exists);
-
-			rm(tmp, function (err) {
-				t.assert(!err);
-			});
 		});
 	});
 });
@@ -45,7 +36,7 @@ test('return path to binary and verify that it is working', function (t) {
 });
 
 test('minify a PNG', function (t) {
-	t.plan(6);
+	t.plan(4);
 
 	var args = [
 		'--recompress',
@@ -54,23 +45,15 @@ test('minify a PNG', function (t) {
 		path.join(__dirname, 'tmp/test.png')
 	];
 
-	mkdir(tmp, function (err) {
+	execFile(require('../').path, args, function (err) {
 		t.assert(!err);
 
-		execFile(require('../').path, args, function (err) {
+		fs.stat(path.join(__dirname, 'fixtures/test.png'), function (err, a) {
 			t.assert(!err);
 
-			fs.stat(path.join(__dirname, 'fixtures/test.png'), function (err, a) {
+			fs.stat(path.join(tmp, 'test.png'), function (err, b) {
 				t.assert(!err);
-
-				fs.stat(path.join(tmp, 'test.png'), function (err, b) {
-					t.assert(!err);
-					t.assert(b.size < a.size);
-
-					rm(tmp, function (err) {
-						t.assert(!err);
-					});
-				});
+				t.assert(b.size < a.size);
 			});
 		});
 	});
