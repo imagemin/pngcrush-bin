c0094f0f3503f729442c24fe35d21b6b3dd6681a
Kevin Martensson <kevinmartensson@gmail.com>
2015-04-20 21:30:11 +0200

    Use `mocha` for testing

--- test/test.js
+++ test/test.js
@@ -1,42 +1,46 @@
+/*global afterEach,beforeEach,it*/
 'use strict';
 
+var assert = require('assert');
 var execFile = require('child_process').execFile;
 var fs = require('fs');
 var path = require('path');
 var binCheck = require('bin-check');
 var BinBuild = require('bin-build');
 var compareSize = require('compare-size');
-var test = require('ava');
+var mkdirp = require('mkdirp');
+var rimraf = require('rimraf');
 var tmp = path.join(__dirname, 'tmp');
 
-test('rebuild the pngcrush binaries', function (t) {
-	t.plan(2);
+beforeEach(function () {
+	mkdirp.sync(tmp);
+});
 
+afterEach(function () {
+	rimraf.sync(tmp);
+});
+
+it('rebuild the pngcrush binaries', function (cb) {
 	new BinBuild()
 		.src('http://downloads.sourceforge.net/project/pmt/pngcrush/1.7.85/pngcrush-1.7.85.zip')
 		.cmd('mkdir -p ' + tmp)
 		.cmd('make && mv pngcrush ' + path.join(tmp, 'pngcrush'))
 		.run(function (err) {
-			t.assert(!err, err);
-
-			fs.exists(path.join(tmp, 'pngcrush'), function (exists) {
-				t.assert(exists);
-			});
+			assert(!err);
+			assert(fs.statSync(path.join(tmp, 'pngcrush')).isFile());
+			cb();
 		});
 });
 
-test('return path to binary and verify that it is working', function (t) {
-	t.plan(2);
-
+it('return path to binary and verify that it is working', function (cb) {
 	binCheck(require('../').path, ['-version'], function (err, works) {
-		t.assert(!err, err);
-		t.assert(works);
+		assert(!err);
+		assert(works);
+		cb();
 	});
 });
 
-test('minify a PNG', function (t) {
-	t.plan(3);
-
+it('minify a PNG', function (cb) {
 	var src = path.join(__dirname, 'fixtures/test.png');
 	var dest = path.join(__dirname, 'tmp/test.png');
 	var args = [
@@ -47,11 +51,12 @@ test('minify a PNG', function (t) {
 	];
 
 	execFile(require('../').path, args, function (err) {
-		t.assert(!err, err);
+		assert(!err);
 
 		compareSize(src, dest, function (err, res) {
-			t.assert(!err, err);
-			t.assert(res[dest] < res[src]);
+			assert(!err);
+			assert(res[dest] < res[src]);
+			cb();
 		});
 	});
 });
