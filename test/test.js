186ab14efe3c7b412e3b02d136b7097aaaaf5b79
Shogo Sensui <shogosensui@gmail.com>
2020-05-31 11:39:39 +0900

    Require Node.js 10 (#21)

--- test/test.js
+++ test/test.js
@@ -10,14 +10,14 @@ const compareSize = require('compare-size');
 const pngcrush = require('..');
 
 test('rebuild the pngcrush binaries', async t => {
-	const tmp = tempy.directory();
+	const temporary = tempy.directory();
 
 	await binBuild.url('https://downloads.sourceforge.net/project/pmt/pngcrush/1.8.13/pngcrush-1.8.13.zip', [
-		`mkdir -p ${tmp}`,
-		`make && mv pngcrush ${path.join(tmp, 'pngcrush')}`
+		`mkdir -p ${temporary}`,
+		`make && mv pngcrush ${path.join(temporary, 'pngcrush')}`
 	]);
 
-	t.true(fs.existsSync(path.join(tmp, 'pngcrush')));
+	t.true(fs.existsSync(path.join(temporary, 'pngcrush')));
 });
 
 test('return path to binary and verify that it is working', async t => {
@@ -25,9 +25,9 @@ test('return path to binary and verify that it is working', async t => {
 });
 
 test('minify a PNG', async t => {
-	const tmp = tempy.directory();
+	const temporary = tempy.directory();
 	const src = path.join(__dirname, 'fixtures/test.png');
-	const dest = path.join(tmp, 'test.png');
+	const dest = path.join(temporary, 'test.png');
 	const args = [
 		'--recompress',
 		'--shrink-extra',
@@ -36,7 +36,7 @@ test('minify a PNG', async t => {
 	];
 
 	await execa(pngcrush, args);
-	const res = await compareSize(src, dest);
+	const result = await compareSize(src, dest);
 
-	t.true(res[dest] < res[src]);
+	t.true(result[dest] < result[src]);
 });
